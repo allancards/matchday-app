0101df96d51f33f4a051d35bac86c82976248c56
allancards <allancardspositivo@gmail.com>
2026-08-21 13:22:59 -0300

    fix: corrige importações e build para Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,15 @@
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-// 1. Configura a conexão do banco de dados nativo
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// 2. Instancia o adaptador do Prisma
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
 const adapter = new PrismaPg(pool)
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
-// 3. Passa o adaptador configurado para o cliente
 export const prisma =
   globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
