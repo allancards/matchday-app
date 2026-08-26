5d6021b79a5c335654822aaad1d77703991d40d9
allancards <allancardspositivo@gmail.com>
2026-08-25 22:33:59 -0300

    fix(admin): corrigir validação de data e exportação na action de eventos

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -3,11 +3,13 @@ import type { NextAuthConfig } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 export default {
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      checks: ['none'],
     }),
   ],
   pages: {
